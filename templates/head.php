c00650b6cf62fe13893052a2b51631bd09e10f93
Tim Sheehan <dontfeedthecode@gmail.com>
2018-01-10 17:35:28 +1000

    remove fonts

--- templates/head.php
+++ templates/head.php
@@ -14,7 +14,7 @@ $menu_item_hover_colour = get_field('header_menu_item_hover_colour', $post->ID);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
-    <link href="https://file.myfontastic.com/Xv9FaXvwhw6vB4qbGXsRSY/icons.css" rel="stylesheet">
+    <!--<link href="https://file.myfontastic.com/Xv9FaXvwhw6vB4qbGXsRSY/icons.css" rel="stylesheet">-->
     <link rel="stylesheet" href="https://use.typekit.net/fot2jlp.css">
     <?php wp_head(); ?>
     <script src="//cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
